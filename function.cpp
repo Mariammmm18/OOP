d36e695fe08a8b13efc33f0e5eb307540ce34bf3
Mariammmm18 <129288408+Mariammmm18@users.noreply.github.com>
2024-03-12 07:56:07 +0300

    function.cpp

--- function.cpp
+++ function.cpp
@@ -2,43 +2,27 @@
 #include "function.h"
 #include <cassert>
 
-namespace test
+namespace function
 {
-std::vector<int> map(std::vector<int> numbers, std::function<int(int)> fun)
-{
-    std::vector<int> result;
-    for (int i = 0; i < size(numbers); i++)
+    std::vector<int> map(std::vector<int> numbers, std::function<int(int)> fun1)
     {
-        result.push_back(fun(numbers[i]));
+        std::vector<int> result;
+        for (int i = 0; i < size(numbers); i++)
+        {
+            result.push_back(fun1(numbers[i]));
+        }
+        return result;
     }
-    return result;
-}
-
-std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)> funn)
-{
-    std::vector<int> result;
-    for (int i = 0; i < size(numbers); i++)
+    
+    std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)> fun2)
     {
-        if (funn(numbers[i])==true)
-        result.push_back (numbers[i]);
+        std::vector<int> result;
+        for (int i = 0; i < size(numbers); i++)
+        {
+            if (fun2(numbers[i])==true)
+            result.push_back (numbers[i]);
+        }
+        return result;
     }
-    return result;
-}
 }
 
-void tests()
-{
-    std::vector<int> test = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-    std::vector<int> massiv = { 1, 4, 9, 16, 25, 36, 49, 64, 81 };
-    assert(test::map(test, [](int x) {return x * x; })==massiv);
-
-    std::vector<int> tested = {  2,  4,  6, 8 };
-    assert(test::filter(test, [](int x) {if (x % 2 == 0) return true; else return false; })==tested);
-}
-int main()
-{
-    tests();
-    std::cout << "tests OK";
-    return 0;
-}
