5157760d985462c899b23f759d001aa999ecbf26
Mariammmm18 <129288408+Mariammmm18@users.noreply.github.com>
2024-03-12 07:59:16 +0300

    function.h

--- function.h
+++ function.h
@@ -1,7 +1,8 @@
-#include <vector>
 #include<functional> 
-namespace test
+#include <vector>
+
+namespace functions
 {
-std::vector<int> map(std::vector<int> numbers, std::function<int(int)>);
-std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)>);
+  std::vector<int> map(std::vector<int> numbers, std::function<int(int)>);
+  std::vector<int> filter(std::vector<int> numbers, std::function<bool(int)>);
 }
